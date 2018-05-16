bca4cfcb15c075c70b0bfa0121550af16e1d0132
Tim Aiken <timaiken@Tims-MacBook-Pro.local>
2018-05-15 22:02:01 -0700

    small change

--- data/read_glove.py
+++ data/read_glove.py
@@ -11,15 +11,16 @@ embeddings = np.load('GloVe_wine_5k.npy')
 vocab = np.load("5k_vocab_dict.npy").item()
 inv_vocab = {v: k for k, v in vocab.items()}
 
-idx = vocab['tannin']
+idx = vocab['firm']
 encoding = embeddings[idx,:]
 
 dists = np.sum(np.square(embeddings - encoding), axis=1)
 
 most_related = np.argpartition(dists, 10)[:10]
 
-print("Original word: tannin")
+print("Original word: firm")
 for val in most_related:
+	if val == idx: continue
 	print("Related word: ", inv_vocab[val])
 
 print(most_related)
